2c6131873e84a30bce9d394e9221a1dbb926ab41
CalebBD <benadelcaleb@gmail.com>
2025-11-19 23:05:54 -0600

    Se trabaja en el laboratorio 8 en clase

---
+++ Lab08/supabaseClient.js
@@ -0,0 +1,11 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+// ==========================
+// CONFIGURA TU SUPABASE
+// ==========================
+let SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wdXJneHN4cXZyZGZyeGticnJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1NjExODksImV4cCI6MjA3NjEzNzE4OX0.Xjny_CN8fp0hmqvvlQJCjevN2-7lhvIpXG4cL_xz8f8";
+let SUPABASE_URL = "https://npurgxsxqvrdfrxkbrri.supabase.co";
+
+// Crear cliente una sola vez
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+ 
